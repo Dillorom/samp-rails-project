3492caac92bb6172d7b8cbc694c3181a14d56a63
Dillorom <dabdulloeva@gmail.com>
2018-08-13 10:16:36 -0700

    create users controller actions :new, :show, :create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    
+    def new
+        @user = User.new
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def create 
+        @user = User.create(user_params)
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password_digest)
+    end
+
 end
