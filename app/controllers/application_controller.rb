d35a23914e1bdf74f5f6f3dd270c78bd553a3f95
Dillorom <dabdulloeva@gmail.com>
2018-08-13 11:04:20 -0700

    create login form in sessions view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    def current_user
+        session[:user_id] = user.id
+      end
+    
+    def logged_in
+        current_user
+    end
 end
