2af95aafbdc0d272a3915119cdb2a83397cbe225
Dillorom <dabdulloeva@gmail.com>
2018-08-13 11:43:57 -0700

    create users view show page and create action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    
+
     def new
         @user = User.new
     end
@@ -10,12 +10,18 @@ class UsersController < ApplicationController
 
     def create 
         @user = User.create(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to @user
+        else
+            redirect_to new_user_path
+        end
     end
 
     private
 
     def user_params
-        params.require(:user).permit(:username, :email, :password_digest)
+        params.require(:user).permit(:username, :password, :email)
     end
 
 end
