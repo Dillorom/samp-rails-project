0540f81b15e953693e6631094c02456c4585af01
Dillorom <dabdulloeva@gmail.com>
2018-08-13 14:01:07 -0700

    add logout link to user show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
 
     def destroy
         session.delete :username
-        redirect_to root_path
+        redirect_to login_path
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   get '/auth/facebook/callback' => 'sessions#create'
 
 end
