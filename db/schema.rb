9f1c724e59c2d5148f740791a48d94e32b5621d0
Dillorom <dabdulloeva@gmail.com>
2018-08-14 16:20:42 -0700

    add admin boolean property to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_14_023710) do
+ActiveRecord::Schema.define(version: 2018_08_14_231712) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2018_08_14_023710) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
 end
