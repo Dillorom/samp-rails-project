728fc624a34ad60e4ee1e5759ccda07c0db5c601
Dillorom <dabdulloeva@gmail.com>
2018-08-14 11:03:31 -0700

    create comments controller actions, comments views templates, and nested route for events/comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,25 @@
 class CommentsController < ApplicationController
     def new
+        @comment = Comment.new
     end
 
     def show
+        @comment = Comment.find(params[:id])
     end
 
     def create
+        @comment = Comment.create(comment_params)
+        if @comment.save
+            redirect_to @event.comment
+        else
+            redirect_to @event
+        end
     end
 
     def destroy
+        @comment = Comment.find(params[:id])
+        @comment.delete
+        redirect_to @event
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
-  resources :events
-  
+  resources :events do
+    resources :comments
+  end
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
