9ef17b3c555426aa0a4d77476fcf9f479d0af1d2
Dillorom <dabdulloeva@gmail.com>
2018-08-15 11:11:51 -0700

    add uid column to users table for Facebook omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_14_231712) do
+ActiveRecord::Schema.define(version: 2018_08_15_181023) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2018_08_14_231712) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.integer "uid"
   end
 
 end
