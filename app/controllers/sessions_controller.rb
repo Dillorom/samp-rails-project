6fa119fb964dae65ac2e3f91ca75a26af504e469
Dillorom <dabdulloeva@gmail.com>
2018-08-16 22:18:20 -0700

    define auth in sessions#create and add if statement to cover both simple login/signup form and omniauth - finally it works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,19 @@
 class SessionsController < ApplicationController
     def create
-        #binding.pry
-        # if auth
+        auth = request.env["omniauth.auth"]
+        if auth
             @user = User.from_omniauth(request.env["omniauth.auth"])
             session[:user_id] = @user.id
             render 'users/show', notice: "Signed In"
-    #    else
-    #         @user = User.find_by(email: params[:email])
-    #         if @user && @user.authenticate(params[:password])
-    #             session[:user_id] = @user.id 
-    #             redirect_to  @user
-    #         else
-    #             render '/sessions/new', :notice => "Username/email/password incorrect or can't be blank"
-    #         end
-    #     end 
-
+           else
+            @user = User.find_by(email: params[:email])
+            if @user && @user.authenticate(params[:password])
+                session[:user_id] = @user.id 
+                redirect_to  @user
+            else
+                render '/sessions/new', :notice => "Username/email/password incorrect or can't be blank"
+            end
+        end 
     end
 
     def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
   has_many :comments
   has_many :events, through: :comments
-  #has_secure_password
+  has_secure_password
 
     validates :username, presence: true
     validates :email, presence: true
     #validates :password, presence: true
     #validates :email, uniqueness: true
   
-   def self.from_omniauth(auth)
+  def self.from_omniauth(auth)
       where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
         user.provider = auth.provider
         user.uid = auth.uid
