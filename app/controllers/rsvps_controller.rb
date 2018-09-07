9ca61bcf7ab08fcfcd6cc5529a357c6aa57cda10
Dillorom <dabdulloeva@gmail.com>
2018-09-07 14:58:41 -0700

    final cleaning for the rails project

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -8,33 +8,23 @@ class RsvpsController < ApplicationController
     end
 
     def show
-        #binding.pry
         @event = Event.find_by_id(params[:event_id])
-        @rsvp = @event.rsvps.find(params[:id]) #this seems to be probelm
+        @rsvp = @event.rsvps.find(params[:id]) 
         
     end
 
     def new
-    #binding.pry
-      @event = Event.find_by_id(params[:event_id])
+       @event = Event.find_by_id(params[:event_id])
       @rsvp = Rsvp.new
    
     end
   
     def create
-        #binding.pry
         @event = Event.find_by_id(params[:event_id])
-        #@rsvp = @event.rsvps.new(rsvp_params.merge(user_id: @user.id, event_id: @event.id))  
-        #@rsvp = @event.rsvps.create(rsvp_params) 
         @rsvp = @event.rsvps.create(rsvp_params)
         @rsvp.user = current_user
-      
         if @rsvp.save
-            #binding.pry
             redirect_to event_rsvp_path(@event, @rsvp) 
-            #redirect_to :controller => 'rsvps', :action => 'show'
-            #redirect_to @event
-            #render :action => :show
         else
             redirect_to @event
         end
@@ -46,7 +36,6 @@ class RsvpsController < ApplicationController
       end
     
       def update
-        # research rails update attributes methods
         @event = Event.find_by_id(params[:event_id])
         @rsvp = @event.rsvps.find(params[:id])
         if @rsvp.update_attributes rsvp_params
@@ -55,16 +44,9 @@ class RsvpsController < ApplicationController
             redirect_to @event
         end
       end
-  
-    # def destroy
-    #   @rsvp = Rsvp.find_by_id(params[:id])
-    #   @rsvp.destroy
-    #   redirect_to @event
-    # end
 
     private
     def rsvp_params
-        #binding.pry
         params.require(:rsvp).permit(:attending)
     end
 end
