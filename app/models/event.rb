4a642dcb4577bae17f2f62dd818cde1d283a5bbe
Dillorom <dabdulloeva@gmail.com>
2018-10-04 17:17:32 -0700

    add prev and next links to event show page- in create methods in event model and render in event show page

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,4 +14,35 @@ class Event < ActiveRecord::Base
   scope :past_events,  -> {where('time < ?',  DateTime.now)}
   scope :future_events,  -> {where('time >= ?', DateTime.now)}
 
+  def prev
+    events = event_ids_by_name
+    event_index = events.find_index(id)
+    prev_event = event_index - 1
+
+    if prev_event > 0
+      events[prev_event]
+    else
+      id
+    end
+  end
+
+  def next
+    events = event_ids_by_name
+    event_index = events.find_index(id)
+    next_event = event_index + 1
+
+    if next_event < events.length
+      events[next_event]
+    else
+      id
+    end
+  end
+
+  private 
+  
+  def event_ids_by_name
+    #@event_ids || = 
+    Event.order(name: :asc).pluck(:id)
+  end
+
 end
