5d1e1c102c2815663b0106d241c7498398e348a8
Dillorom <dabdulloeva@gmail.com>
2018-10-10 11:12:56 -0700

    remove forEach from ajax post request in event.js, both get and post requests work now

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,23 +1,16 @@
 $( document ).on('turbolinks:load', function() {
     $("#load_comments").on("click", function(e){
         e.preventDefault();
-        //
         $.ajax({
             method: 'GET',
             url: this.href + ".json",
             data: $(this).serialize()
         }).done(function(data){
-            
-            
             var $ol = $("#comments ol")
             $ol.html("") 
-            
             data.forEach(function(comment){
-                //debugger
-                
                 $ol.append(`<li> ${comment.user.username} says: <em>${comment.content}</em> </li>`);
             })
-            
         });
     })
 
@@ -26,17 +19,12 @@ $( document ).on('turbolinks:load', function() {
         e.preventDefault();
         $.ajax({
             type: ($("input[name='_method']").val() || this.method),
-            url: this.href + ".json",
+            url: this.action + ".json",
             data: $(this).serialize(),
-            dataType: "json", 
             success: function(data){
-                debugger
                 $("#comments").val("");
                 var $ol = $("#comments ol");
-                data.forEach(function(comment){
-                    //debugger
-                    $ol.append(`<li> ${comment.user.username} says: <em>${comment.content}</em> </li>`);
-                })
+                $ol.append(`<li> ${data.user.username} says: <em>${data.content}</em> </li>`);
             }
         });
     });
