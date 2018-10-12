17a8b24a95f1348165c3f50d3c9a4efb398e5e7d
Dillorom <dabdulloeva@gmail.com>
2018-10-12 12:14:16 -0700

    add comments to explain each event listener in event.js

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -4,6 +4,9 @@ $( document ).on('turbolinks:load', function(){
 })
 
 var attachListeners = function() {
+
+    //Loads all comments through AJAX
+
     $("#load_comments").on("click", function(e){
         e.preventDefault();
         $.ajax({
@@ -22,6 +25,7 @@ var attachListeners = function() {
         });
     })
 
+    //Post a comment through AJAX
 
     $("#new_comment").on("submit", function(e){
         e.preventDefault();
@@ -30,7 +34,7 @@ var attachListeners = function() {
             url: this.action + ".json",
             data: $(this).serialize(),
             success: function(data){
-                const comment = new Comment(data);
+                const comment = new Comment(data); // creates a new object from object constructor
                 $("#comments").append(comment.renderComment())
                 $("#sub").removeAttr('disabled') //to clean comment box from the last comment
             }
@@ -47,12 +51,15 @@ var attachListeners = function() {
         this.user.username = attributes["user"]["username"]
     }
     
+    //Sets a method on object prototype to save memory
+
     Comment.prototype.renderComment = function() {
         const $ol = $("#comments ol");
         $ol.append("<li>"+ this.user.username + " says: " +  "<em>" + this.content + "</em> </li>");
         $("#comment_content").val("");
     }
 
+    //Loads the next event page through AJAX
 
     $(".js-next").on("click", function() {
         const nextId = parseInt($(".js-next").attr("event_id")) + 1;
@@ -66,10 +73,10 @@ var attachListeners = function() {
             $(".eventImage").html("Picture: " + data["image"]);
             $(".js-next").attr("event-id", data["id"]);
         });
-        
         return false
       });
 
+      //Loads the previous event page through AJAX
 
       $(".js-prev").on("click", function() {
         const prevId = parseInt($(".js-prev").attr("event_id")) - 1;
@@ -86,7 +93,8 @@ var attachListeners = function() {
         return false
       });
 
-
+      //Loads the list of attendees through AJAX
+      
       $("#load_attendees").on("click", function(e){
         e.preventDefault();
         
