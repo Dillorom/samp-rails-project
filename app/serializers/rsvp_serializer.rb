dc3941a4c8f172561bf35442327a03c283e38414
Dillorom <dabdulloeva@gmail.com>
2018-10-16 12:42:39 -0700

    generate events and rsvp_events serializer

--- app/serializers/rsvp_serializer.rb
+++ app/serializers/rsvp_serializer.rb
@@ -1,4 +1,5 @@
 class RsvpSerializer < ActiveModel::Serializer
   attributes :id, :attending
   belongs_to :user, serializer: RsvpUserSerializer
+  belongs_to :event, serializer: RsvpEventSerializer
 end
