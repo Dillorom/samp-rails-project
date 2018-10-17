bf7b2b7e5fd7767ea80510cc7cb12c5cb40cae68
Dillorom <dabdulloeva@gmail.com>
2018-10-17 11:44:11 -0700

    finish ajax reqeust in user.js, add new div in user show page, add has_many through relationship in user serializer

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -11,7 +11,14 @@ var attachUserListeners = function() {
             method: "GET",
             url: this.href + ".json"
         }).done(function(data){
-            debugger    
+            const $ul = $("#attending_events ul") 
+            $ul.html("") 
+            const new_data = data.attending_events.map(function(event){
+                return event.name})
+            const sorted_data = new_data.sort();
+            sorted_data.forEach(function(event){
+                $ul.append(`<li> ${event} </li> `)
             })
+        })
     })
 }
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,5 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username
   has_many :comments
   has_many :rsvps
+  has_many :attending_events, through: :rsvps, :source => :event
 end
